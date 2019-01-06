4dddf4dcfdef3553dc5cf31fba55a8388c027040
Inan Evin <inanevin@gmail.com>
2019-01-06 05:06:46 +0300

    Update comments

--- LinaEngine/src/Lina/LayerStack.hpp
+++ LinaEngine/src/Lina/LayerStack.hpp
@@ -2,7 +2,7 @@
 Author: Inan Evin
 www.inanevin.com
 
-Copyright 2018 Ýnan Evin
+Copyright 2018 Inan Evin
 
 Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
 
