50d76bca50f2666417f8e39c940178ad14f10048
inanevin <inanevin@gmail.com>
2020-05-17 16:14:29 +0300

    glm includes

--- LinaCommon/include/Utility/Math/Matrix.hpp
+++ LinaCommon/include/Utility/Math/Matrix.hpp
@@ -28,6 +28,7 @@ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
 #include "Plane.hpp"
 #include "Quaternion.hpp"
 #include "Utility/Log.hpp"
+#include "glm/glm.hpp"
 
 namespace LinaEngine
 {
