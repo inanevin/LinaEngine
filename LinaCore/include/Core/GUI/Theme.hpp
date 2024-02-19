652cdc7dd3b5029a5078b4969a2e70b619c86fab
Inan Evin <inanevin@gmail.com>
2024-02-20 00:06:45 +0200

    project: format

--- LinaCore/include/Core/GUI/Theme.hpp
+++ LinaCore/include/Core/GUI/Theme.hpp
@@ -46,10 +46,10 @@ namespace Lina
 {
 	struct ThemeDef
 	{
-        // Darkest
+		// Darkest
 		Color background0 = Color(0.00802f, 0.00802f, 0.01096f, 1.0f);
 		Color background1 = Color(0.00972f, 0.00913f, 0.01229f, 1.0f);
-        Color background2 = Color(0.01681f, 0.01681f, 0.02122f, 1.0f);
+		Color background2 = Color(0.01681f, 0.01681f, 0.02122f, 1.0f);
 
 		Color foreground0 = Color(0.73f, 0.722f, 0.727f, 1.0f);
 		Color foreground1 = Color(0.019f, 0.018f, 0.022f, 1.0f);
