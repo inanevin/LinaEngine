bd1f66d22c62f4c1dbe7ccc3d63f244d45856ceb
inanevin <inanevin@gmail.com>
2025-01-15 22:32:14 +0300

    format: test

--- LinaCore/include/Core/Physics/PhysicsWorld.hpp
+++ LinaCore/include/Core/Physics/PhysicsWorld.hpp
@@ -42,22 +42,20 @@ namespace Lina
 		PhysicsWorld(EntityWorld* world) : m_world(world){};
 		~PhysicsWorld() = default;
 
-        
 		void Simulate();
 		void WaitForSimulation();
-        
-        // Contacts
-        virtual JPH::ValidateResult  OnContactValidate(const JPH::Body &inBody1, const JPH::Body &inBody2, JPH::RVec3Arg inBaseOffset, const JPH::CollideShapeResult &inCollisionResult) override;
-        virtual void            OnContactAdded(const JPH::Body &inBody1, const JPH::Body &inBody2, const JPH::ContactManifold &inManifold, JPH::ContactSettings &ioSettings) override;
-        virtual void            OnContactPersisted(const JPH::Body &inBody1, const JPH::Body &inBody2, const JPH::ContactManifold &inManifold, JPH::ContactSettings &ioSettings) override;
-        virtual void            OnContactRemoved(const JPH::SubShapeIDPair &inSubShapePair) override;
-        
-        
-        // Body
-        virtual void        OnBodyActivated(const JPH::BodyID &inBodyID, uint64 inBodyUserData) override;
-        virtual void        OnBodyDeactivated(const JPH::BodyID &inBodyID, uint64 inBodyUserData) override;
+
+		// Contacts
+		virtual JPH::ValidateResult OnContactValidate(const JPH::Body& inBody1, const JPH::Body& inBody2, JPH::RVec3Arg inBaseOffset, const JPH::CollideShapeResult& inCollisionResult) override;
+		virtual void				OnContactAdded(const JPH::Body& inBody1, const JPH::Body& inBody2, const JPH::ContactManifold& inManifold, JPH::ContactSettings& ioSettings) override;
+		virtual void				OnContactPersisted(const JPH::Body& inBody1, const JPH::Body& inBody2, const JPH::ContactManifold& inManifold, JPH::ContactSettings& ioSettings) override;
+		virtual void				OnContactRemoved(const JPH::SubShapeIDPair& inSubShapePair) override;
+
+		// Body
+		virtual void OnBodyActivated(const JPH::BodyID& inBodyID, uint64 inBodyUserData) override;
+		virtual void OnBodyDeactivated(const JPH::BodyID& inBodyID, uint64 inBodyUserData) override;
+
 	private:
 		EntityWorld* m_world = nullptr;
 	};
 } // namespace Lina
-
